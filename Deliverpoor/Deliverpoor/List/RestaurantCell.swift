5cbfbf6798c85b142d9fdbd50ba8061fe2ed0aae
Alexandre Freire <xandrefreire@gmail.com>
2019-10-13 15:07:08 +0100

    Add Custom cell

---
+++ Deliverpoor/Deliverpoor/List/RestaurantCell.swift
@@ -0,0 +1,36 @@
+//
+//  RestaurantCell.swift
+//  Deliverpoor
+//
+//  Created by Alexandre Freire García on 13/10/2019.
+//  Copyright © 2019 Alexandre Freire García. All rights reserved.
+//
+
+import UIKit
+
+struct RestaurantCellViewModel {
+    let name: String
+    let address: String
+    let distance: String
+    let time: String
+}
+
+final class RestaurantCell: UITableViewCell {
+
+    // MARK: - Properties
+    @IBOutlet private weak var photoView: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var addressLabel: UILabel!
+    @IBOutlet private weak var distanceLabel: UILabel!
+    @IBOutlet private weak var timeLabel: UILabel!
+}
+
+// MARK: - Public interface
+extension RestaurantCell {
+    func update(with viewModel: RestaurantCellViewModel) {
+        nameLabel.text = viewModel.name
+        addressLabel.text = viewModel.distance
+        distanceLabel.text = viewModel.distance
+        timeLabel.text = viewModel.time
+    }
+}
--- Deliverpoor/DeliverpoorPlayground.playground/Pages/CLLocation.xcplaygroundpage/Contents.swift
+++ Deliverpoor/DeliverpoorPlayground.playground/Pages/CLLocation.xcplaygroundpage/Contents.swift
@@ -11,12 +11,14 @@ let riazor = CLLocation(latitude: 43.3687184, longitude: -8.4174835)
 // Pontedeume
 let pontedeume = CLLocation(latitude: 43.4011397, longitude: -8.1767528)
 
-// Calculamos la distancia 
+// Calculamos la distancia
 let distance = pontedeume.distance(from: riazor) // En metros, línea recta
 
 // Transformamos las unidades de longitud
 var measurement = Measurement(value: distance, unit: UnitLength.meters)
 measurement.convert(to: UnitLength.kilometers)
+
+
 
 
 
