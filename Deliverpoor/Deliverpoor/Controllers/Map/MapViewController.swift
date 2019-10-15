b3a72c1c235ff200de45fe240d054e1878e6404f
Alexandre Freire <xandrefreire@gmail.com>
2019-10-15 16:17:28 +0100

    Add annotations to the map. Restaurant conforms to MKAnnotation protocol

--- Deliverpoor/Deliverpoor/Controllers/Map/MapViewController.swift
+++ Deliverpoor/Deliverpoor/Controllers/Map/MapViewController.swift
@@ -11,25 +11,30 @@ import MapKit
 
 final class MapViewController: UIViewController {
 
-    // MARK: - Properties
+    // MARK: - Outlets
     @IBOutlet private weak var mapView: MKMapView!
     @IBOutlet private weak var segmentedControl: UISegmentedControl!
     
+    // MARK: - Properties
+    private let repository: RestaurantRepositoryProtocol
+    
     // MARK: - Initialization
-      init() {
-          super.init(nibName: nil, bundle: nil)
+    init(repository: RestaurantRepositoryProtocol = LocalRestaurantRepository()) {
+        self.repository = repository
+        super.init(nibName: nil, bundle: nil)
         title = "Map"
-      }
-      
-      required init?(coder: NSCoder) {
-          fatalError("init(coder:) has not been implemented")
-      }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
         centerMapInACoruna()
+        mapView.addAnnotations(repository.all())
     }
     
     // MARK: - Actions
