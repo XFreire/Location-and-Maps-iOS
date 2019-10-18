4d1eb57ae2631c85f20559ba281f95a8a52b9b40
Alexandre Freire <xandrefreire@gmail.com>
2019-10-18 21:34:15 +0100

    Add CLLocation playground page

--- Deliverpoor/DeliverpoorPlayground.playground/Pages/CLLocation.xcplaygroundpage/Contents.swift
+++ Deliverpoor/DeliverpoorPlayground.playground/Pages/CLLocation.xcplaygroundpage/Contents.swift
@@ -6,6 +6,22 @@ import CoreLocation
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+// Creamos varios objetos CLLocation
+
+// Estadio de Riazor
+let riazor = CLLocation(latitude: 43.3687184, longitude: -8.4174835)
+
+// Pontedeume
+let pontedeume = CLLocation(latitude: 43.4011397, longitude: -8.1767528)
+
+// Calculamos la distancia
+let distance = pontedeume.distance(from: riazor) // En metros, línea recta
+
+// Transformamos las unidades de longitud
+var measurement = Measurement(value: distance, unit: UnitLength.meters)
+measurement.convert(to: UnitLength.kilometers)
+measurement.convert(to: UnitLength.miles)
+measurement.convert(to: UnitLength.feet)
 
 
 //: [Next](@next)
